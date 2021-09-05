7a9b9aa4e04985cb72cc9a4e29c23f1960d99bbb
Toshinori Sumida <t.sumida1987@gmial.com>
2021-09-06 02:25:07 +0900

    useState test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React, { useState } from 'react'
+
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount((previousCount) => count + 1)
+  const decrement2 = () => setCount((previousCount) => count - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount((previousCount) => count * 2)
+  const divide3 = () =>
+    setCount((previousCount) => {
+      return count % 3 === 0 ? previousCount / 3 : previousCount
+    })
+
   return (
-    <div>
-      <p>This is a template for React app.</p>
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>*2</button>
+        <button onClick={divide3}>3の倍数の時のみ3で割る</button>
+      </div>
+    </>
   )
 }
 
